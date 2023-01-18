a82b85596188f4e553f4bb50be7daec6210778a9
Aly <alyshariff2810@gmail.com>
2023-01-17 23:33:09 -0500

    1st problem

---
+++ ride_problem.py
@@ -0,0 +1,17 @@
+"""
+ID: alyshar1
+LANG: PYTHON3
+PROG: ride
+"""
+import sys
+sys.stderr.write('message')
+fin = open ('ride.in', 'r')
+fout = open ('ride.out', 'w')
+x = map(str, fin.readline().split())
+y = map(str, fin.readline().split())
+
+def ride():
+    pass
+
+fout.write ("")
+fout.close()
